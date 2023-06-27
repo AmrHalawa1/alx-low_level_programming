a76c844205064d95241782f4405a3ad746b2d329
amrhalawa1 <amrhalawa98@gmail.com>
2023-06-27 16:49:47 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,5 +1,5 @@
+#include<stdio.h>
 #include "main.h"
-#include <stdio.h>
 /**
  *print_array - update value.
  *@a: value to be evaluate.
@@ -12,7 +12,11 @@ void print_array(int *a, int n)
 
 	for (x = 0 ; x < n; x++)
 	{
-		printf("%d, ", a[x]);
+		printf("%d", a[x]);
+		if (x != n - 1)
+		{
+			printf(", ");
+		}
 	}
 	printf("\n");
 }
